b05e0a83514f69a6ae6dd22f2e2c6ccab9155304
aadarshjha <jha.aadarsh@gmail.com>
2022-02-15 11:21:41 -0600

    commenting out to debug

--- static/plot.py
+++ static/plot.py
@@ -19,23 +19,23 @@ def plot_training_validation(train_loss, train_acc, experiment_name):
     axs[0,1].set_xlabel('Epoch')
     axs[0,1].set_ylabel('Loss')
 
-    axs[0,2].plot(train_loss[2], label='train_loss')
-    axs[0,2].set_title('Fold 3')
+    # axs[0,2].plot(train_loss[2], label='train_loss')
+    # axs[0,2].set_title('Fold 3')
 
-    axs[0,2].set_xlabel('Epoch')
-    axs[0,2].set_ylabel('Loss')
+    # axs[0,2].set_xlabel('Epoch')
+    # axs[0,2].set_ylabel('Loss')
 
-    axs[1,0].plot(train_loss[3], label='train_loss')
-    axs[1,0].set_title('Fold 4')
+    # axs[1,0].plot(train_loss[3], label='train_loss')
+    # axs[1,0].set_title('Fold 4')
 
-    axs[1,0].set_xlabel('Epoch')
-    axs[1,0].set_ylabel('Loss')
+    # axs[1,0].set_xlabel('Epoch')
+    # axs[1,0].set_ylabel('Loss')
 
-    axs[1,1].plot(train_loss[4], label='train_loss')
-    axs[1,1].set_title('Fold 5')
+    # axs[1,1].plot(train_loss[4], label='train_loss')
+    # axs[1,1].set_title('Fold 5')
 
-    axs[1,1].set_xlabel('Epoch')
-    axs[1,1].set_ylabel('Loss')
+    # axs[1,1].set_xlabel('Epoch')
+    # axs[1,1].set_ylabel('Loss')
 
     # remove the subplot at the bottom right
     fig.delaxes(axs[1,2])
@@ -70,24 +70,24 @@ def plot_training_validation(train_loss, train_acc, experiment_name):
     axs[0,1].set_xlabel('Epoch')
     axs[0,1].set_ylabel('Accuracy')
 
-    axs[0,2].plot(train_acc[2], label='train_acc')
-    axs[0,2].set_title('Fold 3')
+    # axs[0,2].plot(train_acc[2], label='train_acc')
+    # axs[0,2].set_title('Fold 3')
 
-    axs[0,2].set_xlabel('Epoch')
-    axs[0,2].set_xlabel('Accuracy')
+    # axs[0,2].set_xlabel('Epoch')
+    # axs[0,2].set_xlabel('Accuracy')
 
 
-    axs[1,0].plot(train_acc[3], label='train_acc')
-    axs[1,0].set_title('Fold 4')
+    # axs[1,0].plot(train_acc[3], label='train_acc')
+    # axs[1,0].set_title('Fold 4')
 
-    axs[1,0].set_xlabel('Epoch')
-    axs[1,0].set_xlabel('Accuracy')
+    # axs[1,0].set_xlabel('Epoch')
+    # axs[1,0].set_xlabel('Accuracy')
 
-    axs[1,1].plot(train_acc[4], label='train_acc')
-    axs[1,1].set_title('Fold 5')
+    # axs[1,1].plot(train_acc[4], label='train_acc')
+    # axs[1,1].set_title('Fold 5')
 
-    axs[1,1].set_xlabel('Epoch')
-    axs[1,1].set_xlabel('Accuracy')
+    # axs[1,1].set_xlabel('Epoch')
+    # axs[1,1].set_xlabel('Accuracy')
 
     # remove the subplot at the bottom right
     fig.delaxes(axs[1,2])
