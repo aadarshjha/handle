f81eae663efb0864d62386c2c324b00067d44b5f
aadarshjha <jha.aadarsh@gmail.com>
2022-02-14 21:36:27 -0600

    more logging

--- static/main.py
+++ static/main.py
@@ -117,6 +117,15 @@ def execute_training(X, y, experiment_name = 'exper1', num_folds=5, epochs=10, b
         validation_loss.append(scores[0])
         validation_acc.append(scores[1])
 
+        # print the train loss and accuracy
+        print("Train loss: ", history.history['loss'])
+        print("Train accuracy: ", history.history['accuracy'])
+
+        # print the test loss and accuracy
+        print("Test loss: ", scores[0])
+        print("Test accuracy: ", scores[1])
+
+
     return model_cache, train_loss, train_acc, validation_loss, validation_acc
     
 def execute_testing(model_cache, X, y, experiment_name='exper1'):
