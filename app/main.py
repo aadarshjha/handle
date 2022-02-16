957dabbf384e04cb3e0bfb466e1874c876e52153
aadarshjha <jha.aadarsh@gmail.com>
2022-02-16 09:54:50 -0600

    checekin code for app

--- app/main.py
+++ app/main.py
@@ -4,9 +4,23 @@ from PyQt5.QtWidgets import QApplication, QLabel
 class MainWindow(QApplication):
     def __init__(self):
         super().__init__([])
-        self.window = QLabel("Hello World")
-        self.window.show()
+        # bigger window size:
+        self.window_size = (1000, 800)
+        
+    # function to create the camera window: 
+    def create_camera_window(self):
+        self.camera_window = QLabel("Camera Window")
+        self.camera_window.show()
+        self.camera_window.setFixedSize(self.window_size[0], self.window_size[1])
         self.exec_()
 
-if __name__ == "main":
-    MainWindow()
+
+if __name__ == "__main__":
+    MainWindow().create_camera_window()
+    MainWindow().execute_window()
+
+# # create a new window
+# app = QApplication([])
+# window = QLabel("Hello World")
+# window.show()
+# app.exec_()
