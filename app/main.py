247251d94d0a940f3407de12c43c84e0ac723906
aadarshjha <jha.aadarsh@gmail.com>
2022-02-21 20:49:20 -0600

    cheecking in application code

--- app/main.py
+++ app/main.py
@@ -1,26 +1,63 @@
-from PyQt5.QtWidgets import QApplication, QLabel 
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout
+from PyQt6.QtGui import QPalette, QColor
+import sys 
+import json
+
+# read ./version.json
+with open('./version.json', 'r') as f:
+    version = json.load(f)
 
 
-class MainWindow(QApplication):
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+
+
+class MainWindow(QMainWindow): 
     def __init__(self):
-        super().__init__([])
-        # bigger window size:
-        self.window_size = (1000, 800)
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Handle: {} | {}".format(version.get('version'), 'Development' if version.get('development') else 'Stable'))
+
+        # set fixed size of window
+        self.setFixedSize(1250, 820)
+        self.setLayout()
+    
+    def setLayout(self):
+
+        # horizontal layout
+        layout = QHBoxLayout()
+
+        layout.addWidget(self.left())
+        layout.addWidget(Color('blue'))
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def left(self): 
+        left_widget = QWidget(self)
+        left_widget.setAutoFillBackground(True)
+        # set width to 40% and height to 100%
         
-    # function to create the camera window: 
-    def create_camera_window(self):
-        self.camera_window = QLabel("Camera Window")
-        self.camera_window.show()
-        self.camera_window.setFixedSize(self.window_size[0], self.window_size[1])
-        self.exec_()
+        return left_widget
+
+    def right(self):
+        right_widget = QWidget(self)
+        right_widget.setAutoFillBackground(True)
+        # set width to 
+        return right_widget
 
 
 if __name__ == "__main__":
-    MainWindow().create_camera_window()
-    MainWindow().execute_window()
-
-# # create a new window
-# app = QApplication([])
-# window = QLabel("Hello World")
-# window.show()
-# app.exec_()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
