6c3c6a7f6caf6de659beb601bb671d81675b8105
aadarshjha <jha.aadarsh@gmail.com>
2022-02-21 21:06:10 -0600

    placement of camera

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
 from PyQt6.QtGui import QPalette, QColor
 import sys 
 import json
@@ -21,8 +21,6 @@ class Color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.ColorRole.Window, QColor(color))
         self.setPalette(palette)
-
-
 
 class MainWindow(QMainWindow): 
     def __init__(self):
@@ -50,12 +48,38 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(widget)
 
+    def remove_spacing(self, layout): 
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
     def left(self): 
         left_widget = QWidget(self)
         left_widget.setAutoFillBackground(True)
         # set width to 40% and height to 100% of window
         left_widget.setFixedSize(int(WIDTH * 0.4), HEIGHT)
-        left_widget.setStyleSheet("background-color: blue")
+        # left_widget.setStyleSheet("background-color: blue")
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(Color('Blue'))
+        layout.addWidget(Color('Green'))
+
+        # remove margin and spacing
+        self.remove_spacing(layout)
+
+        left_widget.setLayout(layout)
+
+
+        # create camera element in top quarter of widget
+        # camera = Color('#00ff00')
+        # camera.setFixedSize(int(WIDTH * 0.4), int(HEIGHT * 0.4))
+        # camera.setStyleSheet("background-color: #00ff00")
+
+
+
+        # left_widget.layout = Q
+
+
         return left_widget
 
     def right(self):
