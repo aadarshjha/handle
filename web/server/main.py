def7ec75a634c668704100724756335a5ed713b0
aadarshjha <jha.aadarsh@gmail.com>
2022-02-24 15:30:13 -0600

    somethings wrong with these requests

--- web/server/main.py
+++ web/server/main.py
@@ -9,14 +9,25 @@ cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
 # accept a JSON object
-@app.route("/static/cnn", methods=["GET", "POST"])
+@app.route("/static/cnn", methods=["POST", "GET"])
 def index():
     data = request.get_json()
     image = Process(data["imageSrc"]).readb64()
-    preprocessed = Inference(image).augment_single_image()
-    preprocessed = Inference(image).convert_to_b64()
-    return preprocessed
-    # return to the client the preprocessed image
+    if request.method == "POST":
+        return ""
+    elif request.method == "GET":
+        preprocessed = Inference(image).augment_single_image()
+        preprocessed = Inference(image).convert_to_b64()
+        return preprocessed
+    else:
+        # POST error 405
+        print("Method not allowed")
+
+
+# return a JSON object
+@app.route("/", methods=["GET"])
+def label():
+    return {"label": "hello"}
 
 
 if __name__ == "__main__":
