e877a0a763a35b1130d1c9eb69bf02f3c30fe2a0
aadarshjha <jha.aadarsh@gmail.com>
2022-02-27 10:47:01 -0600

    reformat

--- static/main.py
+++ static/main.py
@@ -5,129 +5,175 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import sys 
+import sys
 import yaml
 import pandas as pd
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers import Dense, Flatten
 import json
 from sklearn.model_selection import train_test_split
 
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-gpus= tf.config.experimental.list_physical_devices('GPU')
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+gpus = tf.config.experimental.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(gpus[0], True)
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 PREFIX = "../../drive/MyDrive/handleData/"
 
 # custom plotting
-from plot import * 
+from plot import *
 
 # collect the data
-def read_data(): 
+def read_data():
     imagepaths = []
-    for root, _, files in os.walk("../data/leapGestRecog", topdown=False): 
+    for root, _, files in os.walk("../data/leapGestRecog", topdown=False):
         for name in files:
             path = os.path.join(root, name)
-            if path.endswith("png"): # We want only the images
+            if path.endswith("png"):  # We want only the images
                 imagepaths.append(path)
 
-    # printing the number of images loaded: 
+    # printing the number of images loaded:
     print("Number of images loaded: ", len(imagepaths))
-    return imagepaths 
+    return imagepaths
 
-def augment_data(imagepaths): 
 
-    X = [] 
-    y = [] 
+def augment_data(imagepaths):
+
+    X = []
+    y = []
 
     # Loops through imagepaths to load images and labels into arrays
     for path in imagepaths:
-        img = cv2.imread(path) # Reads image and returns np.array
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converts into the corret colorspace (GRAY)
-        img = cv2.resize(img, (320, 120)) # Reduce image size so training can be faster
+        img = cv2.imread(path)  # Reads image and returns np.array
+        img = cv2.cvtColor(
+            img, cv2.COLOR_BGR2GRAY
+        )  # Converts into the corret colorspace (GRAY)
+        img = cv2.resize(img, (320, 120))  # Reduce image size so training can be faster
         X.append(img)
-    
+
         # Processing label in image path
         category = path.split("/")[3]
-        label = int(category.split("_")[0][1]) # We need to convert 10_down to 00_down, or else it crashes
+        label = int(
+            category.split("_")[0][1]
+        )  # We need to convert 10_down to 00_down, or else it crashes
         y.append(label)
 
     X = np.array(X, dtype="uint8")
-    X = X.reshape(len(imagepaths), 120, 320, 1) 
+    X = X.reshape(len(imagepaths), 120, 320, 1)
     y = np.array(y)
 
     print("Images loaded: ", len(X))
     print("Labels loaded: ", len(y))
 
-    return X, y 
+    return X, y
+
 
 def create_model(mode="CNN"):
 
-    model = None 
+    model = None
     if mode == "CNN":
         model = Sequential()
-        model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(120, 320, 1))) 
+        model.add(Conv2D(32, (5, 5), activation="relu", input_shape=(120, 320, 1)))
         model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu')) 
+        model.add(Conv2D(64, (3, 3), activation="relu"))
         model.add(MaxPooling2D((2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
         model.add(MaxPooling2D((2, 2)))
         model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(10, activation='softmax'))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dense(10, activation="softmax"))
     elif mode == "RESNET":
-        model = keras.applications.resnet.ResNet50(include_top=False, weights=None, input_shape=(120, 320, 1))
-    elif mode == "RESNET_PRETRAINED": 
-        model = keras.applications.resnet.ResNet50(include_top=False, weights='imagenet', input_shape=(120, 320, 1))
-    elif mode == "MOBILENET": 
-        model = keras.applications.mobilenet.MobileNet(include_top=False, weights=None, input_shape=(120, 320, 1))
+        model = keras.applications.resnet.ResNet50(
+            include_top=False, weights=None, input_shape=(120, 320, 1)
+        )
+    elif mode == "RESNET_PRETRAINED":
+        model = keras.applications.resnet.ResNet50(
+            include_top=False, weights="imagenet", input_shape=(120, 320, 1)
+        )
+    elif mode == "MOBILENET":
+        model = keras.applications.mobilenet.MobileNet(
+            include_top=False, weights=None, input_shape=(120, 320, 1)
+        )
     elif mode == "MOBILENET_PRETRAINED":
-        model = keras.applications.mobilenet.MobileNet(include_top=False, weights='imagenet', input_shape=(120, 320, 1))
+        model = keras.applications.mobilenet.MobileNet(
+            include_top=False, weights="imagenet", input_shape=(120, 320, 1)
+        )
     elif mode == "DENSENET":
-        model = keras.applications.densenet.DenseNet121(include_top=False, weights=None, input_shape=(120, 320, 1))
-    else: 
-        # throw an error to the user 
-        raise Exception("Invalid model t    ype")
+        model = keras.applications.densenet.DenseNet121(
+            include_top=False, weights=None, input_shape=(120, 320, 1)
+        )
+    elif mode == "DENSENET_PRETRAINED":
+        model = keras.applications.densenet.DenseNet121(
+            include_top=False, weights="imagenet", input_shape=(120, 320, 1)
+        )
+    else:
+        # throw an error to the user
+        raise Exception("Invalid model type")
 
-    return model 
+    return model
 
 
-def execute_training(X, y, experiment_name = 'exper1', num_folds=5, epochs=10, batch_size=32, verbose=False, optimizer='adam', loss='sparse_categorical_crossentropy', mode='CNN'):
+def execute_training(
+    X,
+    y,
+    experiment_name="exper1",
+    num_folds=5,
+    epochs=10,
+    batch_size=32,
+    verbose=False,
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    mode="CNN",
+):
 
     kfold = KFold(n_splits=num_folds, shuffle=True)
 
     fold_no = 1
 
     model_cache = []
-    predictions_cache = [] 
-    targets_cache = [] 
+    predictions_cache = []
+    targets_cache = []
 
-    train_loss = [] 
+    train_loss = []
     train_acc = []
 
-    val_loss = [] 
-    val_acc = [] 
+    val_loss = []
+    val_acc = []
 
-    precision_history = [] 
+    precision_history = []
     recall_history = []
     f1_history = []
     accuracy_history = []
-    cfx_history = [] 
+    cfx_history = []
 
     # train across folds
     for train, test in kfold.split(X, y):
 
         model = create_model()
-        model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])  
+        model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
 
         print("For Fold: " + str(fold_no))
-        X_val, X_test, y_val, y_test = train_test_split(X[test], y[test], test_size=0.5, random_state=42)
+        X_val, X_test, y_val, y_test = train_test_split(
+            X[test], y[test], test_size=0.5, random_state=42
+        )
 
-        history = model.fit(X[train], y[train], batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_val, y_val))
+        history = model.fit(
+            X[train],
+            y[train],
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            validation_data=(X_val, y_val),
+        )
         scores = model.evaluate(X[test], y[test], verbose=verbose)
 
         # save the predictions from the model.evaluate
@@ -135,9 +181,9 @@ def execute_training(X, y, experiment_name = 'exper1', num_folds=5, epochs=10, b
         predictions = y_prob.argmax(axis=-1)
 
         # compute the precision, recall, f1 score, and accuracy
-        precision = precision_score(y_test, predictions, average='weighted')
-        recall = recall_score(y_test, predictions, average='weighted')
-        f1 = f1_score(y_test, predictions, average='weighted')
+        precision = precision_score(y_test, predictions, average="weighted")
+        recall = recall_score(y_test, predictions, average="weighted")
+        f1 = f1_score(y_test, predictions, average="weighted")
         accuracy = accuracy_score(y_test, predictions)
 
         # cache the target values
@@ -146,30 +192,29 @@ def execute_training(X, y, experiment_name = 'exper1', num_folds=5, epochs=10, b
         # create a confusion matrix
         cfx = confusion_matrix(y_test, predictions)
         cfx_history.append(cfx)
-        
+
         # cache the above
         precision_history.append(precision)
         recall_history.append(recall)
         f1_history.append(f1)
         accuracy_history.append(accuracy)
 
-        # cache the predictions for micro use 
+        # cache the predictions for micro use
         predictions_cache.append(predictions)
 
-        # save the training loss and accuracy 
-        train_loss.append(history.history['loss'])
-        train_acc.append(history.history['accuracy'])
+        # save the training loss and accuracy
+        train_loss.append(history.history["loss"])
+        train_acc.append(history.history["accuracy"])
 
         # save the validation loss and accuracy
-        val_loss.append(history.history['val_loss'])
-        val_acc.append(history.history['val_accuracy'])
+        val_loss.append(history.history["val_loss"])
+        val_acc.append(history.history["val_accuracy"])
 
         # print the test loss and test accuracy
         print("Test loss: ", scores[0])
         print("Test accuracy: ", scores[1])
 
-
-        # print the metrics: 
+        # print the metrics:
         print("Precision: ", precision)
         print("Recall: ", recall)
         print("F1: ", f1)
@@ -183,18 +228,42 @@ def execute_training(X, y, experiment_name = 'exper1', num_folds=5, epochs=10, b
 
         fold_no += 1
 
-    return model_cache, train_loss, train_acc, val_loss, val_acc, predictions_cache, targets_cache, precision_history, recall_history, f1_history, accuracy_history, cfx_history
-    
-def execute_micro_macro_metrics(model_cache, predictions_cache, targets_cache, precision_history, recall_history, f1_history, accuracy_history, cfx_history, experiment_name):
+    return (
+        model_cache,
+        train_loss,
+        train_acc,
+        val_loss,
+        val_acc,
+        predictions_cache,
+        targets_cache,
+        precision_history,
+        recall_history,
+        f1_history,
+        accuracy_history,
+        cfx_history,
+    )
 
-    epoch_counter = 1 
+
+def execute_micro_macro_metrics(
+    model_cache,
+    predictions_cache,
+    targets_cache,
+    precision_history,
+    recall_history,
+    f1_history,
+    accuracy_history,
+    cfx_history,
+    experiment_name,
+):
+
+    epoch_counter = 1
     for model in model_cache:
         model.save(PREFIX + "models/{}/{}.h5".format(experiment_name, epoch_counter))
         epoch_counter = epoch_counter + 1
-    
+
     JSON_data = {}
 
-    # macro averaging: 
+    # macro averaging:
     macro_precision = np.mean(precision_history)
     macro_recall = np.mean(recall_history)
     macro_f1 = np.mean(f1_history)
@@ -202,57 +271,126 @@ def execute_micro_macro_metrics(model_cache, predictions_cache, targets_cache, p
     cfx_history_avg = np.mean(cfx_history, axis=0)
     cfx_history_avg_json = json.dumps(cfx_history_avg.tolist())
 
-    # store into JSON: 
-    JSON_data['macro_metrics'] = {'precision': macro_precision, 'recall': macro_recall, 'f1': macro_f1, 'accuracy': macro_accuracy, 'cfx': cfx_history_avg_json}
+    # store into JSON:
+    JSON_data["macro_metrics"] = {
+        "precision": macro_precision,
+        "recall": macro_recall,
+        "f1": macro_f1,
+        "accuracy": macro_accuracy,
+        "cfx": cfx_history_avg_json,
+    }
 
     # micro averaging:
 
-    micro_precision = precision_score(np.concatenate(targets_cache), np.concatenate(predictions_cache), average="weighted")
-    micro_recall = recall_score(np.concatenate(targets_cache), np.concatenate(predictions_cache), average="weighted")
-    micro_f1 = f1_score(np.concatenate(targets_cache), np.concatenate(predictions_cache), average="weighted")
-    micro_accuracy = accuracy_score(np.concatenate(targets_cache), np.concatenate(predictions_cache))
+    micro_precision = precision_score(
+        np.concatenate(targets_cache),
+        np.concatenate(predictions_cache),
+        average="weighted",
+    )
+    micro_recall = recall_score(
+        np.concatenate(targets_cache),
+        np.concatenate(predictions_cache),
+        average="weighted",
+    )
+    micro_f1 = f1_score(
+        np.concatenate(targets_cache),
+        np.concatenate(predictions_cache),
+        average="weighted",
+    )
+    micro_accuracy = accuracy_score(
+        np.concatenate(targets_cache), np.concatenate(predictions_cache)
+    )
 
     # confusion matrix for micro averaging:
-    cfx_micro = confusion_matrix(np.concatenate(targets_cache), np.concatenate(predictions_cache))
+    cfx_micro = confusion_matrix(
+        np.concatenate(targets_cache), np.concatenate(predictions_cache)
+    )
     cfx_micro_json = json.dumps(cfx_micro.tolist())
-    
+
     # store into JSON:
-    JSON_data['micro_metrics'] = {'precision': micro_precision, 'recall': micro_recall, 'f1': micro_f1, 'accuracy': micro_accuracy, 'cfx': cfx_micro_json}
+    JSON_data["micro_metrics"] = {
+        "precision": micro_precision,
+        "recall": micro_recall,
+        "f1": micro_f1,
+        "accuracy": micro_accuracy,
+        "cfx": cfx_micro_json,
+    }
 
     # save the JSON_data to a file
-    with open(PREFIX + "logs/" + experiment_name + "/" + experiment_name + "_data.json", 'w') as outfile:
+    with open(
+        PREFIX + "logs/" + experiment_name + "/" + experiment_name + "_data.json", "w"
+    ) as outfile:
         json.dump(JSON_data, outfile)
 
-def extract_hyperparameters(filename): 
-    hyperparams = None 
-    with open("experiments/{}.yaml".format(filename), "r") as f: 
+
+def extract_hyperparameters(filename):
+    hyperparams = None
+    with open("experiments/{}.yaml".format(filename), "r") as f:
         hyperparameters = yaml.load(f, Loader=yaml.FullLoader)
     return hyperparameters
 
+
 if __name__ == "__main__":
 
-    # extract file name from command line input 
+    # extract file name from command line input
     filename = sys.argv[1]
     hyperparameters = extract_hyperparameters(filename)
 
-    if not os.path.exists(PREFIX + 'models/' + hyperparameters["EXPERIMENT_NAME"]):
-        os.makedirs(PREFIX + 'models/' + hyperparameters["EXPERIMENT_NAME"])
+    if not os.path.exists(PREFIX + "models/" + hyperparameters["EXPERIMENT_NAME"]):
+        os.makedirs(PREFIX + "models/" + hyperparameters["EXPERIMENT_NAME"])
 
-    if not os.path.exists(PREFIX + 'logs/' + hyperparameters["EXPERIMENT_NAME"]):
-        os.makedirs(PREFIX + 'logs/' + hyperparameters["EXPERIMENT_NAME"])
+    if not os.path.exists(PREFIX + "logs/" + hyperparameters["EXPERIMENT_NAME"]):
+        os.makedirs(PREFIX + "logs/" + hyperparameters["EXPERIMENT_NAME"])
 
     # read the data
     imagepaths = read_data()
 
-    # finalize data 
+    # finalize data
     X, y = augment_data(imagepaths)
 
     # execute the training pipeline
-    model_cache, train_loss, train_acc, val_loss, val_acc, predictions_cache, targets_cache, precision_history, recall_history, f1_history, accuracy_history, cfx_history = \
-        execute_training(X, y, hyperparameters["EXPERIMENT_NAME"], 
-        hyperparameters["CONFIG"]["NUM_FOLDS"], hyperparameters["CONFIG"]["EPOCHS"], 
-        hyperparameters["CONFIG"]["BATCH_SIZE"], hyperparameters["CONFIG"]["VERBOSE"], 
-        hyperparameters["CONFIG"]["OPTIMIZER"], hyperparameters["CONFIG"]["LOSS"], hyperparameters["CONFIG"]["MODE"])
-    
-    plot_training_validation(train_loss, train_acc, val_loss, val_acc, hyperparameters["EXPERIMENT_NAME"], PREFIX)
-    execute_micro_macro_metrics(model_cache, predictions_cache, targets_cache, precision_history, recall_history, f1_history, accuracy_history, cfx_history, hyperparameters["EXPERIMENT_NAME"])
+    (
+        model_cache,
+        train_loss,
+        train_acc,
+        val_loss,
+        val_acc,
+        predictions_cache,
+        targets_cache,
+        precision_history,
+        recall_history,
+        f1_history,
+        accuracy_history,
+        cfx_history,
+    ) = execute_training(
+        X,
+        y,
+        hyperparameters["EXPERIMENT_NAME"],
+        hyperparameters["CONFIG"]["NUM_FOLDS"],
+        hyperparameters["CONFIG"]["EPOCHS"],
+        hyperparameters["CONFIG"]["BATCH_SIZE"],
+        hyperparameters["CONFIG"]["VERBOSE"],
+        hyperparameters["CONFIG"]["OPTIMIZER"],
+        hyperparameters["CONFIG"]["LOSS"],
+        hyperparameters["CONFIG"]["MODE"],
+    )
+
+    plot_training_validation(
+        train_loss,
+        train_acc,
+        val_loss,
+        val_acc,
+        hyperparameters["EXPERIMENT_NAME"],
+        PREFIX,
+    )
+    execute_micro_macro_metrics(
+        model_cache,
+        predictions_cache,
+        targets_cache,
+        precision_history,
+        recall_history,
+        f1_history,
+        accuracy_history,
+        cfx_history,
+        hyperparameters["EXPERIMENT_NAME"],
+    )
--- static/plot.py
+++ static/plot.py
@@ -1,49 +1,52 @@
 import matplotlib.pyplot as plt
-import os 
+import os
 
-def plot_training_validation(train_loss, train_acc, val_loss, val_acc, experiment_name, PREFIX):
+
+def plot_training_validation(
+    train_loss, train_acc, val_loss, val_acc, experiment_name, PREFIX
+):
 
     # plot the train_loss vs validation_loss in a 5 subplots
     fig, axs = plt.subplots(2, 3)
 
-    axs[0,0].plot(train_loss[0], label='train_loss')
-    axs[0,0].plot(val_loss[0], label='val_loss')
-    axs[0,0].set_title('Fold 1')
+    axs[0, 0].plot(train_loss[0], label="train_loss")
+    axs[0, 0].plot(val_loss[0], label="val_loss")
+    axs[0, 0].set_title("Fold 1")
 
     # set the x axis as epoch and y axis as loss
-    axs[0,0].set_xlabel('Epoch')
-    axs[0,0].set_ylabel('Loss')
+    axs[0, 0].set_xlabel("Epoch")
+    axs[0, 0].set_ylabel("Loss")
 
-    axs[0,1].plot(train_loss[1], label='train_loss')
-    axs[0,1].plot(val_loss[1], label='val_loss')
-    axs[0,1].set_title('Fold 2')
+    axs[0, 1].plot(train_loss[1], label="train_loss")
+    axs[0, 1].plot(val_loss[1], label="val_loss")
+    axs[0, 1].set_title("Fold 2")
 
-    axs[0,1].set_xlabel('Epoch')
-    axs[0,1].set_ylabel('Loss')
+    axs[0, 1].set_xlabel("Epoch")
+    axs[0, 1].set_ylabel("Loss")
 
-    axs[0,2].plot(train_loss[2], label='train_loss')
-    axs[0,2].plot(val_loss[2], label='val_loss')
-    axs[0,2].set_title('Fold 3')
+    axs[0, 2].plot(train_loss[2], label="train_loss")
+    axs[0, 2].plot(val_loss[2], label="val_loss")
+    axs[0, 2].set_title("Fold 3")
 
-    axs[0,2].set_xlabel('Epoch')
-    axs[0,2].set_ylabel('Loss')
+    axs[0, 2].set_xlabel("Epoch")
+    axs[0, 2].set_ylabel("Loss")
 
-    axs[1,0].plot(train_loss[3], label='train_loss')
-    axs[1,0].plot(val_loss[3], label='val_loss')
-    axs[1,0].set_title('Fold 4')
+    axs[1, 0].plot(train_loss[3], label="train_loss")
+    axs[1, 0].plot(val_loss[3], label="val_loss")
+    axs[1, 0].set_title("Fold 4")
 
-    axs[1,0].set_xlabel('Epoch')
-    axs[1,0].set_ylabel('Loss')
+    axs[1, 0].set_xlabel("Epoch")
+    axs[1, 0].set_ylabel("Loss")
 
-    axs[1,1].plot(train_loss[4], label='train_loss')
-    axs[1,1].plot(val_loss[4], label='val_loss')
-    axs[1,1].set_title('Fold 5')
+    axs[1, 1].plot(train_loss[4], label="train_loss")
+    axs[1, 1].plot(val_loss[4], label="val_loss")
+    axs[1, 1].set_title("Fold 5")
 
-    axs[1,1].set_xlabel('Epoch')
-    axs[1,1].set_ylabel('Loss')
+    axs[1, 1].set_xlabel("Epoch")
+    axs[1, 1].set_ylabel("Loss")
 
     # remove the subplot at the bottom right
-    fig.delaxes(axs[1,2])
+    fig.delaxes(axs[1, 2])
 
     # increase the spacing between subplots
     fig.tight_layout(pad=2.0)
@@ -55,7 +58,14 @@ def plot_training_validation(train_loss, train_acc, val_loss, val_acc, experimen
     fig.set_size_inches(20, 10)
 
     # save the image and create folder if it doesn't exist
-    plt.savefig(PREFIX + "logs/" + experiment_name + "/training_loss_" + experiment_name + ".png")
+    plt.savefig(
+        PREFIX
+        + "logs/"
+        + experiment_name
+        + "/training_loss_"
+        + experiment_name
+        + ".png"
+    )
 
     # clear the plot
     plt.clf()
@@ -63,44 +73,43 @@ def plot_training_validation(train_loss, train_acc, val_loss, val_acc, experimen
     # do the same thing with train_acc and validation_acc
     fig, axs = plt.subplots(2, 3)
 
-    axs[0,0].plot(train_acc[0], label='train_acc')
-    axs[0,0].plot(val_acc[0], label='val_acc')
-    axs[0,0].set_title('Fold 1')
+    axs[0, 0].plot(train_acc[0], label="train_acc")
+    axs[0, 0].plot(val_acc[0], label="val_acc")
+    axs[0, 0].set_title("Fold 1")
 
-    axs[0,0].set_xlabel('Epoch')
-    axs[0,0].set_ylabel('Accuracy')
+    axs[0, 0].set_xlabel("Epoch")
+    axs[0, 0].set_ylabel("Accuracy")
 
-    axs[0,1].plot(train_acc[1], label='train_acc')
-    axs[0,1].plot(val_acc[1], label='val_acc')
-    axs[0,1].set_title('Fold 2')
+    axs[0, 1].plot(train_acc[1], label="train_acc")
+    axs[0, 1].plot(val_acc[1], label="val_acc")
+    axs[0, 1].set_title("Fold 2")
 
-    axs[0,1].set_xlabel('Epoch')
-    axs[0,1].set_ylabel('Accuracy')
+    axs[0, 1].set_xlabel("Epoch")
+    axs[0, 1].set_ylabel("Accuracy")
 
-    axs[0,2].plot(train_acc[2], label='train_acc')
-    axs[0,2].plot(val_acc[2], label='val_acc')
-    axs[0,2].set_title('Fold 3')
+    axs[0, 2].plot(train_acc[2], label="train_acc")
+    axs[0, 2].plot(val_acc[2], label="val_acc")
+    axs[0, 2].set_title("Fold 3")
 
-    axs[0,2].set_xlabel('Epoch')
-    axs[0,2].set_xlabel('Accuracy')
+    axs[0, 2].set_xlabel("Epoch")
+    axs[0, 2].set_xlabel("Accuracy")
 
+    axs[1, 0].plot(train_acc[3], label="train_acc")
+    axs[1, 0].plot(val_acc[3], label="val_acc")
+    axs[1, 0].set_title("Fold 4")
 
-    axs[1,0].plot(train_acc[3], label='train_acc')
-    axs[1,0].plot(val_acc[3], label='val_acc')
-    axs[1,0].set_title('Fold 4')
+    axs[1, 0].set_xlabel("Epoch")
+    axs[1, 0].set_xlabel("Accuracy")
 
-    axs[1,0].set_xlabel('Epoch')
-    axs[1,0].set_xlabel('Accuracy')
+    axs[1, 1].plot(train_acc[4], label="train_acc")
+    axs[1, 1].plot(val_acc[4], label="val_acc")
+    axs[1, 1].set_title("Fold 5")
 
-    axs[1,1].plot(train_acc[4], label='train_acc')
-    axs[1,1].plot(val_acc[4], label='val_acc')
-    axs[1,1].set_title('Fold 5')
-
-    axs[1,1].set_xlabel('Epoch')
-    axs[1,1].set_xlabel('Accuracy')
+    axs[1, 1].set_xlabel("Epoch")
+    axs[1, 1].set_xlabel("Accuracy")
 
     # remove the subplot at the bottom right
-    fig.delaxes(axs[1,2])
+    fig.delaxes(axs[1, 2])
 
     # increase the spacing between subplots
     fig.tight_layout(pad=2.0)
@@ -113,7 +122,9 @@ def plot_training_validation(train_loss, train_acc, val_loss, val_acc, experimen
 
     # save the image
 
-    plt.savefig(PREFIX + "logs/" + experiment_name + "/training_acc_" + experiment_name + ".png")
+    plt.savefig(
+        PREFIX + "logs/" + experiment_name + "/training_acc_" + experiment_name + ".png"
+    )
 
     # clear the plot
     plt.clf()
