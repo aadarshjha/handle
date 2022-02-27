9b570fc1e5c15f80e9993f369e9cd31df362fb10
aadarshjha <jha.aadarsh@gmail.com>
2022-02-27 10:56:48 -0600

    pretrained

--- static/main.py
+++ static/main.py
@@ -84,10 +84,13 @@ def create_model(mode="CNN"):
         model = Sequential()
         model.add(Conv2D(32, (5, 5), activation="relu", input_shape=(120, 320, 1)))
         model.add(MaxPooling2D((2, 2)))
+
         model.add(Conv2D(64, (3, 3), activation="relu"))
         model.add(MaxPooling2D((2, 2)))
+
         model.add(Conv2D(64, (3, 3), activation="relu"))
         model.add(MaxPooling2D((2, 2)))
+
         model.add(Flatten())
         model.add(Dense(128, activation="relu"))
         model.add(Dense(10, activation="softmax"))
